b5582415df9983b30944478a7360ee6b065dc4e8
MadeleineSmith <madeleinesmith1995@gmail.com>
2019-02-07 08:09:17 +0000

    Change dbup + dbdown scripts to stop concatenating all migrations to a massive temporary file

--- scripts/dbup.sh
+++ scripts/dbup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
+DB_NAME=${1:-coupons_test}
+DB_PASSWORD=${2:-testingtesting123}
+DB_USER=${3:-testing}
+
 ROOT_DIR_PATH=$(cd $(dirname $0)/.. && pwd)
 
 for FILE in `ls -1 ${ROOT_DIR_PATH}/db/migrations/*up.sql`
 do
-  cat ${FILE} >> /tmp/allfiles.sql
+  echo "Executing: `basename ${FILE}`"
+  psql "dbname='${DB_NAME}' password='${DB_PASSWORD}' user='${DB_USER}'" -f ${FILE} 2>allfiles.err.log
 done
-
-DB_NAME=$1
-DB_USER=$2
-
-psql -d ${DB_NAME} -U ${DB_USER} -f /tmp/allfiles.sql 2>allfiles.err.log
-
-rm /tmp/allfiles.sql
