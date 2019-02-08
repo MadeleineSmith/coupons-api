93edfb10a319285ff64ca6f16904489545e9bb30
MadeleineSmith <madeleinesmith1995@gmail.com>
2019-02-08 22:26:06 +0000

    Change dbup/ dbdown scripts to write error log to root level file

--- scripts/dbup.sh
+++ scripts/dbup.sh
@@ -11,5 +11,5 @@ ROOT_DIR_PATH=$(cd $(dirname $0)/.. && pwd)
 for FILE in `ls -1 ${ROOT_DIR_PATH}/db/migrations/*up.sql`
 do
   echo "Executing: `basename ${FILE}`"
-  psql "dbname='${DB_NAME}' password='${DB_PASSWORD}' user='${DB_USER}'" -f ${FILE} 2>allfiles.err.log
+  psql "dbname='${DB_NAME}' password='${DB_PASSWORD}' user='${DB_USER}'" -f ${FILE} 2>${ROOT_DIR_PATH}/allfiles.err.log
 done
